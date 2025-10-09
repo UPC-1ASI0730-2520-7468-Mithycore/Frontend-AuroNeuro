e822754d3b8d06e4e55b2ed4e89fb497cb8aab63
jhimyromeromeza <romeromezajhimy0@gmail.com>
2025-10-09 16:43:51 -0500

    feat(otp-auth): add view otp auth

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router/router'
-import 'primeicons/primeicons.css'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router/router";
+import PrimeVue from "primevue/config";
+
+// Estilos globales de PrimeVue
+import Aura from '@primeuix/themes/material';  
+import "primeflex/primeflex.css"; // Opcional, si usas las utilidades flex
+import "primeicons/primeicons.css";
 
 createApp(App)
+    .use(PrimeVue, {
+        theme: {
+            preset: Aura
+        }
+    })
     .use(router)
-    .mount('#app')
+    .mount("#app");
--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import MainLayout from "../layout/main-layout/presentation/main-layout.vue";
 import LoginLayout from "../layout/login-layout/presentation/login-layout.vue";
 import RegisterUsers from "../register-users/presentation/register-users.vue";
+import OtpAuth from "../shared/phone-auth/presentation/otp-auth.vue";
 
 
 const routes = [
