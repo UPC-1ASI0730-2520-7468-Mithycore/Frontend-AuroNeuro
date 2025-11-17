01a08c0d979599b6967059bfd4c2a09b8ecefc52
jhimyromeromeza <romeromezajhimy0@gmail.com>
2025-11-16 21:45:27 -0500

    feat: add vercel.json and change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+
+    // ⚠️ Fix para Vercel: evita que esbuild rompa vue.runtime.esm-bundler.js
+    optimizeDeps: {
+        exclude: ['vue']
+    },
+
+    build: {
+        commonjsOptions: {
+            include: []
+        }
+    }
 })
